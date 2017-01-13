ef5e987c7cbea757efbfdce9b4cf87c2c1f91fed
Imad Ajallal <dopest@Imads-MacBook-Pro.local>
2017-01-13 23:08:16 +0000

    Orgaized

--- TravCo/SignInViewController.swift
+++ TravCo/SignInViewController.swift
@@ -9,28 +9,12 @@
 import UIKit
 import Firebase
 import GoogleSignIn
-
+import FirebaseAuth
+import FirebaseDatabase
 class SignInViewController: UIViewController,GIDSignInUIDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance().uiDelegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
